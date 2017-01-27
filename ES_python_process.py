14a82d4c62ae4e9a44f347eb9f87fc88ece18015
asoa <abailey82@gmail.com>
2017-01-27 02:48:46 -0500

    updated ES_python_process.py

--- ES_python_process.py
+++ ES_python_process.py
@@ -3,59 +3,96 @@
 from __future__ import print_function
 from datetime import datetime
 from elasticsearch import Elasticsearch
+import argparse
+import sys
+import time
+import csv
 
 client = Elasticsearch()
 
-# json request was derived from Kibana dispayed request; click on " ^ " under histogram on Discover tab
-response = client.search(
-    index="logstash*",
-    body={
-        "size": 500,  # specify how many records to return that match the query
 
-        "sort": [  # sort records in desc order based on time field
-            {
-              "@timestamp": {
-                "order": "desc",
-                "unmapped_type": "boolean"
-              }
-            }
-          ],
+def query_elasticsearch(query, q_size, start_time, end_time):  # json request was derived from Kibana dispayed request; click on " ^ " under histogram on Discover tab
+    response = client.search(
+        index="logstash*",
+        body={
+            "size": q_size,  # specify how many records to return that match the query
 
-        "query": {
-            "bool": {
-              "must": [
+            "sort": [  # sort records in desc order based on time field
                 {
-                  "query_string": {
-                    "query": "*",
-                  }
-                },
-                {
-                  "match": {
-                    "type": {
-                      "query": "process",  # only return records labeled "process"
-                      "type": "phrase"
-                    }
-                  }
-                },
-                {
-                  "range": {
-                    "@timestamp": {
-                      "gte": 1482484753418,
-                      "lte": 1482658453418,
-                      "format": "epoch_millis"
-                    }
+                  "@timestamp": {
+                    "order": "desc",
+                    "unmapped_type": "boolean"
                   }
                 }
               ],
-              "must_not": []
+
+            "query": {
+                "bool": {
+                  "must": [
+                    {
+                      "query_string": {
+                        "query": query,  # lucene formatted query, default is *
+                      }
+                    },
+                    {
+                      "match": {
+                        "type": {
+                          "query": "process",  # only return records labeled "process"
+                          "type": "phrase"
+                        }
+                      }
+                    },
+                    {
+                      "range": {
+                        "@timestamp": {
+                          "gte": start_time,  # return records within the specified time parameters
+                          "lte": end_time,
+                          "format": "epoch_millis"
+                        }
+                      }
+                    }
+                  ],
+                  "must_not": []
+                }
             }
         }
-    }
 
-)
+    )
+    return response
 
-for hit in response['hits']['hits']:
-    print(hit["_source"]["TimeCreated"],hit["_source"]["NewProcessName"],hit["_source"]["CommandLine"])
+
+def get_epoch(s_time, e_time):  # convert date format to epoch
+    pattern = '%d %m %Y %H:%M:%S'
+    start_time = int(time.mktime(time.strptime(s_time, pattern)))
+    end_time = int(time.mktime(time.strptime(e_time, pattern)))
+    return str(start_time) + "000", str(end_time) + "000"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(usage=sys.argv[0] + ' -q <lucene query> -s <query_size> -s_time <start_time> -e_time <end_time>', description="enter the following cmd line arguments:")
+    parser.add_argument("-q", dest="query", default="*", help="lucene formatted query, default is '*'")
+    parser.add_argument("-s", dest="q_size", default=500, help="max amount of records query will return, default=500")
+    parser.add_argument("-s_time", dest="s_time", help="start time of query, format: DD MM YY HH:MM")
+    parser.add_argument("-e_time", dest="e_time", help="end time of query, format: DD MM YY HH:MM")
+    args = parser.parse_args()
+    if args.query is None or args.q_size is None or args.s_time is None or args.e_time is None:
+        print(parser.usage)
+        exit(0)
+    return args.query, args.q_size, args.s_time, args.e_time
+
+
+def main():
+    query, size, s_time, e_time = parse_args()
+    start_time, end_time = get_epoch(s_time, e_time)
+    response = query_elasticsearch(query, size, start_time, end_time)
+    for hit in response['hits']['hits']:
+        hit_tuple = (hit["_source"]["TimeCreated"], hit["_source"]["NewProcessName"], hit["_source"]["CommandLine"])
+        print(hit_tuple)
+        #print(hit["_source"]["TimeCreated"], hit["_source"]["NewProcessName"], hit["_source"]["CommandLine"])
+
+if __name__ == "__main__":
+    main()
+
 
 # Output returned to console from query " * "  to index logstash* matching on all documents tagged with "process" label
 
@@ -69,9 +106,9 @@ for hit in response['hits']['hits']:
 12/24/2016 8:31:51 AM C:\Windows\System32\calc.exe C:\Windows\System32\calc.exe
 """
 
-#TODO: create argparse function to create cmd line argmuments: q_size, d_range
-#TODO: create function to convert d_range argument to epoch time passed to range key in json query
 #TODO: create function to match on processess not residing in C:\Windows\System32\ and output to file
 #TODO: create function tokenize process name only and check against white|black lists
 #TODO: create function to count occurences of process and output to file
+#TODO: create function to set start|end default time to last 24 hours
+#TODO: output hits to csv file
 
